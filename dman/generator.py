d065ac01b3e1fa5a1679dd7ecd88af13c51980a9
PeterCoppens <petercoppens@skynet.be>
2021-10-13 11:27:27 +0200

    more data management utils and run loading

--- dman/generator.py
+++ dman/generator.py
@@ -1,13 +1,17 @@
+from abc import ABC, abstractmethod
 import argparse
 import configparser
+import logging
 import os
+from re import I
 import shutil
 import sys
 import json
 
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import List
+from typing import Dict, List
+from collections import ChainMap, OrderedDict
 
 from dman.stamp import Manager
 from dman.utils import prompt_user
@@ -15,9 +19,10 @@ from dman.utils import prompt_user
 
 @dataclass
 class TimingEntry:
+    DATESTR = '%d/%m/%y %H:%M:%S'
+
     begin: datetime
     end: datetime = field(default=None)
-    datestr: str = field(default='%d/%m/%y %H:%M:%S')
     
     @property
     def finished(self):
@@ -30,15 +35,22 @@ class TimingEntry:
     def stop(self):
         self.end = datetime.now()
     
-    def export(self, _: str):
+    def export(self, _: str = None):
         end = 'not finished'
         if self.end:
-            end = self.end.strftime(self.datestr)
+            end = self.end.strftime(TimingEntry.DATESTR)
             
         return {
-            'start': self.begin.strftime(self.datestr),
+            'start': self.begin.strftime(TimingEntry.DATESTR),
             'end': end
         }
+
+    @classmethod
+    def load(cls, path: str, record: dict):
+        return cls(
+            begin=datetime.strptime(record['start'], TimingEntry.DATESTR),
+            end=datetime.strptime(record['end'], TimingEntry.DATESTR)
+        )
 
     @staticmethod
     def configure_parser(_: argparse.ArgumentParser):
@@ -66,6 +78,10 @@ class ConfigEntry:
             return {
                 'config': ConfigEntry.DEFAULT_CONFIG
             }
+    
+    @classmethod
+    def load(cls, path: str, record: dict):
+        return cls.from_path(os.path.join(path, record['config']))
 
     @staticmethod
     def configure_parser(parser: argparse.ArgumentParser):
@@ -116,6 +132,14 @@ class RunEntry:
             'description': self.msg
         }
     
+    @classmethod
+    def load(cls, path: str, record: dict):
+        return cls(
+            name=record['name'],
+            stamp=record['stamp'],
+            msg=record['description']
+        )
+    
     @staticmethod
     def configure_parser(parser: argparse.ArgumentParser):
         parser.add_argument(
@@ -163,8 +187,6 @@ class ScriptsEntry:
     local: bool = field(default=False)
     
     def export(self, target: str):
-        mgr = Manager()
-
         if self.local:
             local_gen = os.path.join(target, ScriptsEntry.DEFAULT_GENERATOR)
             shutil.copy(src=self.gen, dst=local_gen)
@@ -176,14 +198,32 @@ class ScriptsEntry:
                 self.parsers[i] = os.path.basename(local_parser)
         else:
             for i, parser in enumerate(self.parsers):
-                self.parsers[i] = os.path.relpath(parser, start=mgr.root_path)
-            self.gen = os.path.relpath(self.gen, start=mgr.root_path)
+                self.parsers[i] = os.path.relpath(parser, start=target)
+            self.gen = os.path.relpath(self.gen, start=target)
 
         return {
             'generator': self.gen,
             'parsers': json.dumps(self.parsers)
         }
-            
+    
+    @classmethod
+    def load(cls, path: str, record: dict):
+        path = os.path.realpath(path)
+
+        parsers = json.loads(record['parsers'])
+        for i, p in enumerate(parsers):
+            parsers[i] = os.path.realpath(os.path.join(path, p))
+        
+        genpath = os.path.realpath(os.path.join(path, record['generator']))
+
+        # check if first part of genpath is path (TODO better test?)
+        islocal = genpath.find(path) == 0  
+
+        return cls(
+            gen=genpath,
+            parsers=parsers,
+            local=islocal
+        )      
 
     @staticmethod
     def configure_parser(parser: argparse.ArgumentParser):
@@ -215,12 +255,107 @@ class ScriptsEntry:
         return cls(gen=gen, parsers=parsers, local=args.local)
 
 
+# source https://medium.com/@vadimpushtaev/python-choosing-subclass-cf5b1b67c696
+class DataType(ABC):
+    data_types = {}
+    type: str = 'Base'
+
+    def __init__(self, file, use_basename=True):
+        if use_basename:
+            file = os.path.basename(file)
+        self.file = file
+
+    @classmethod
+    def register(cls, data_type): 
+        def decorator(subclass):
+            subclass.type = data_type
+            cls.data_types[data_type] = subclass
+            return subclass
+        
+        return decorator
+    
+    def export(self, target):
+        self.write(target)
+        return {'type': self.type, 'file': self.file}
+    
+    @classmethod
+    def load(cls, path: str, record: dict):
+        if record['type'] not in cls.data_types:
+            raise ValueError(f'Bad class type {record["type"]}')
+        
+        return cls.data_types[record['type']].from_file(os.path.join(path, record['file']))
+    
+    @classmethod
+    @abstractmethod
+    def from_file(cls, file: str):
+        raise NotImplementedError('not implemented')
+
+    @abstractmethod
+    def write(self, target: str):
+        raise NotImplementedError('not implemented')
+
+
+
+@DataType.register('cluster')
+class DataCluster(DataType, OrderedDict):
+    DEFAULT_CLUSTER = 'cluster.ini'
+
+    def __init__(self, folder: str, use_basename = True, content: OrderedDict = None):
+        DataType.__init__(self, folder, use_basename)
+        if content is None:
+            content = {}
+        OrderedDict.__init__(self, **content)
+    
+    def append(self, entry: 'DataEntry'):
+        self[entry.name] = entry
+    
+    @classmethod
+    def from_file(cls, file: str):
+        record = configparser.ConfigParser()
+        record.read(os.path.join(file, DataCluster.DEFAULT_CLUSTER))
+        content = DataEntry.load(file, record['Data'])
+        return cls(folder=file, content=content)
+    
+    def write(self, target: str):
+        record = configparser.ConfigParser()
+        path = os.path.join(target, self.file)
+
+        os.mkdir(path)
+        record['Data'] = dict(ChainMap(*reversed([data.export(path) for data in self.values()])))
+
+        with open(os.path.join(path, DataCluster.DEFAULT_CLUSTER), 'w') as configfile:
+            record.write(configfile)
+
+
+@dataclass
+class DataEntry:
+    name: str
+    description: str
+    content: DataType = field(compare=False)
+
+    @abstractmethod
+    def export(self, target: str):
+        return {self.name : json.dumps({'description': self.description, **self.content.export(target)})}
+    
+    @classmethod
+    def load(cls, path, record):
+        res = {}
+        for name, v in record.items():
+            rec = json.loads(v)
+            res[name] = cls(
+                name = name, 
+                description = rec['description'],
+                content=DataType.load(path, rec)
+            )
+        return res
+
+
 class Run:
     DEFAULT_RECORD = 'info.ini'
     DEFAULT_RUN_DATE_STRING = '%y-%m-%d-%H%M%S'
     DEFAULT_RUN_TARGET = '.dman/runs'
 
-    def __init__(self, target: str, meta: RunEntry, scripts: ScriptsEntry, config: ConfigEntry = None, timing: TimingEntry = None) -> None:
+    def __init__(self, target: str, meta: RunEntry, scripts: ScriptsEntry, config: ConfigEntry = None, timing: TimingEntry = None, data: Dict[str, DataEntry] = None, override: bool = False) -> None:
         self.target = target
 
         self.meta = meta
@@ -233,18 +368,57 @@ class Run:
         self.finished = False
         self.exported = False
 
+        self.override = override
+
+        if data is None:
+            data = {}
+        self.data = data
+
+    def append(self, data: DataEntry):
+        self.data[data.name] = data
+
+    @property
+    def path(self):
+        return os.path.join(self.target, self.meta.name)
+
     def export(self):
-        target = os.path.join(self.target, self.meta.name)
-        if not os.path.exists(target):
-            os.makedirs(target)
+        path = self.path
+        if os.path.exists(path):
+            if self.override:
+                path = path + '.tmp'
+            else:
+                logging.error('could not export run: {path} exists.')
+                return None
+
+        os.makedirs(path)      # path should not exist at this point
 
         record = configparser.ConfigParser()
-        record['Info'] = {**self.meta.export(target), **self.config.export(target)}
-        record['Scripts'] = self.scripts.export(target)
-        record['Timing'] = self.timing.export(target)
+        record['Info'] = {**self.meta.export(path), **self.config.export(path)}
+        record['Scripts'] = self.scripts.export(path)
+        record['Timing'] = self.timing.export(path)
+        record['Data'] = dict(ChainMap(*[data.export(path) for data in self.data.values()]))
 
-        with open(os.path.join(target, Run.DEFAULT_RECORD), 'w') as configfile:
-            record.write(configfile)        
+        with open(os.path.join(path, Run.DEFAULT_RECORD), 'w') as configfile:
+            record.write(configfile)
+
+        if path != self.path and self.override:
+            shutil.rmtree(self.path)
+            shutil.move(src=path, dst=self.path)                        
+        
+        return self.path
+
+    @classmethod
+    def load(cls, path):
+        record = configparser.ConfigParser()
+        record.read(os.path.join(path, Run.DEFAULT_RECORD))
+
+        meta = RunEntry.load(path, record['Info'])
+        config = ConfigEntry.load(path, record['Info'])
+        scripts = ScriptsEntry.load(path, record['Scripts'])
+        timing = TimingEntry.load(path, record['Timing'])
+        data = DataEntry.load(path, record['Data'])
+
+        return cls(target=os.path.realpath(os.path.dirname(path)), meta=meta, config=config, scripts=scripts, timing=timing, data=data)
 
     @staticmethod
     def configure_parser(parser: argparse.ArgumentParser):
@@ -264,8 +438,7 @@ class Run:
         )
 
         for entry in [RunEntry, ScriptsEntry, TimingEntry, ConfigEntry]:
-            entry.configure_parser(parser)
-            
+            entry.configure_parser(parser) 
     
     @classmethod
     def from_argument_parser(cls, parser, args, generator_name: str = None):
@@ -280,17 +453,81 @@ class Run:
         meta = RunEntry.from_argument_parser(parser, args, default_name=default_name)
 
         dest = os.path.join(target, meta.name)
-        if os.path.exists(dest):
-            if args.override and prompt_user(f'override folder {dest}'):
-                shutil.rmtree(dest)
-                # TODO proper remove of run (including of the stamp?)
-            else:
+        if os.path.exists(dest) and not args.override:
                 raise parser.error(f'Output folder already exists: {dest}. Use --override if you want to overwrite its contents.')
             
         scripts = ScriptsEntry.from_argument_parser(parser, args)
         config = ConfigEntry.from_argument_parser(parser, args)
         timing = TimingEntry.from_argument_parser(parser, args, default_start=current_time)
-        return cls(target=target, meta=meta, scripts=scripts, config=config, timing=timing)
+        return cls(target=target, meta=meta, scripts=scripts, config=config, timing=timing, override=args.override)
+        
+
+class GeneratorRecord:
+    DEFAULT_GEN_FILE = '.dman/gen.ini'
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+        self.mgr = Manager()
+        if not self.mgr.valid:
+            sys.exit()
+
+        self.config_path = os.path.join(self.mgr.root_path, GeneratorRecord.DEFAULT_GEN_FILE)
+        self.config = configparser.ConfigParser()
+        if os.path.exists(self.config_path):
+            self.config.read(self.config_path)
+        
+        if not self.config.has_section(self.name):
+            self.config[self.name] = {
+                'latest': '', 'time': '', 'runs': json.dumps([])
+            }
+        
+        self.runs : List[str] = json.loads(self.config[self.name]['runs'])
+
+    def save(self):
+        with open(self.config_path, 'w') as configfile:
+            self.config.write(configfile)
+    
+    def update(self, path: str, time: str):
+        path = os.path.relpath(path, start=self.mgr.root_path)
+
+        # store new run in path (or push to last element)
+        if path in self.runs:
+            self.runs.remove(path)
+        self.runs.append(path)
+
+        # update config for this generator
+        self.config[self.name] = {
+            'latest': path,
+            'time': time,
+            'runs': json.dumps(self.runs)
+        }
+        self.save()
+    
+    @property
+    def latest(self):
+        return self.config[self.name]['latest']
+
+
+class LatestRun:
+    def __init__(self, gen_name=str, update: bool = False) -> None:
+        self.record = GeneratorRecord(gen_name)
+        self.run = None
+        self.update = update
+
+    def __enter__(self):
+        self.run = Run.load(self.record.latest)
+        return self.run
+    
+    def __exit__(self, exc_type, exc_val, exc_trb):
+        if exc_type is not None:
+            if not prompt_user('Process encountered an error, do you still want to update the run?'):
+                return
+
+        if self.update:
+            self.run.override = True
+            path = self.run.export()
+            self.record.update(path, time=self.run.timing.export()['start'])
         
 
 class Generator:
@@ -302,10 +539,7 @@ class Generator:
 
     def __init__(self, name: str) -> None:
         self.name = name
-
-        self.mgr = Manager()
-        if not self.mgr.valid:
-            sys.exit()
+        self.record = GeneratorRecord(name)
 
         self.parser = argparse.ArgumentParser(f'{name} generator')
         Run.configure_parser(self.parser)
@@ -313,7 +547,7 @@ class Generator:
     def parse(self):
         self.args = self.parser.parse_args()
         self.run = Run.from_argument_parser(self.parser, self.args, generator_name=f'{self.name}')
-        
+
     def __enter__(self):
         self.parse()
         self.run.timing.start
@@ -322,4 +556,9 @@ class Generator:
     def __exit__(self, exc_type, exc_val, exc_trb):
         if exc_type is None:
             if not self.run.timing.finished: self.run.timing.stop()
-        self.run.export()
+
+        # export run
+        path = self.run.export()
+
+        # record new run
+        self.record.update(path, time=self.run.timing.export()['start'])
