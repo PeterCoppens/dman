02d911e456226cf2e11191786911994dcc931a4c
PeterCoppens <petercoppens@skynet.be>
2022-02-17 16:53:45 +0100

    removed import of verbose.setup in dman.__init__

--- dman/__init__.py
+++ dman/__init__.py
@@ -4,7 +4,7 @@ from dman.persistent.serializables import serializable, serialize, deserialize, 
 from dman.persistent.serializables import ser_type2str, ser_str2type
 from dman.persistent.serializables import isvalid
 from dman.persistent.record import record,  remove
-from dman.verbose import context, setup
+from dman.verbose import context
 from dman.persistent.storables import storable, write, read
 from dman.persistent.configclasses import configclass, section
 from dman.repository import track, save, load
